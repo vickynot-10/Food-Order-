be06d85c015a39ce699f077eab8e46e65415292c
Vicky-clowdy <brianvickyx@gmail.com>
2024-10-02 19:01:58 +0530

    env to production

--- Backend/server.js
+++ Backend/server.js
@@ -4,13 +4,28 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import { getDatabase } from './Database/database.js';
 import routes from './Routes/routes.js';
+import path,{dirname} from 'path';
+
 getDatabase()
 import { configDotenv } from 'dotenv';
+import { fileURLToPath } from 'url';
 configDotenv()
 app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true
 }))
+const __filename=fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
+if(process.env.ENVIRONMENT=='production'){
+    app.use(express.static(path.join(__dirname,'..','Frontend','build')));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(path.join(__dirname,'..','Frontend','build','index.html')))
+    })
+    
+}
+
+
+
 app.use(express.json());
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
