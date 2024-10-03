6a8277359ea3c0fecaf7e814b15491fe76e486b7
Vicky-clowdy <brianvickyx@gmail.com>
2024-10-03 10:02:34 +0530

    Removed path

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,13 @@
-import express from 'express';
+import express from 'express'
 const app = express()
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import { getDatabase } from './Database/database.js';
 import routes from './Routes/routes.js';
-import path,{dirname} from 'path';
 
 getDatabase()
 
 import { configDotenv } from 'dotenv';
-import { fileURLToPath } from 'url';
 
 configDotenv()
 
@@ -17,20 +15,6 @@ app.use(cors({
     origin: true,
     credentials: true
 }))
-
-
-const __filename=fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename)
-
-if(process.env.ENVIRONMENT=='production'){
-    app.use(express.static(path.join(__dirname,'..','Frontend','build')));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(path.join(__dirname,'..','Frontend','build','index.html')))
-    })
-    
-}
-
-
 
 app.use(express.json());
 app.use(cookieParser())
